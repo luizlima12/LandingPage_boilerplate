d0ed021436682da4c5a7a7f2e51ecc25e3e59736
luizlima12 <adonaiiluiz55@gmail.com>
2025-05-03 18:24:21 -0300

    Adiciona novos componentes e atualiza dependências para melhorar a estrutura do projeto

---
+++ src/components/header/header.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+import Logo from "../../../public/next.svg";
+export default function Header() {
+  return (
+    <header className="flex items-center justify-between p-4 bg-primary text-white">
+      <Image
+        src={Logo}
+        alt="Logo"
+        width={100}
+        height={100}
+        className="h-8 w-auto shadow-xl"
+      />
+    </header>
+  );
+}
---
+++ src/components/header/nav-bar.tsx
@@ -0,0 +1,5 @@
+export default function NavBar() {
+  return (
+    <nav className="flex items-center justify-between p-4 bg-primary text-white"></nav>
+  );
+}
