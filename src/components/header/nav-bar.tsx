e27f3a9737f7a508ca2a0f83ae7c11b733aa8c8b
luizlima12 <adonaiiluiz55@gmail.com>
2025-05-05 17:01:15 -0300

    Adiciona componente Sheet com funcionalidades de diálogo e animações; atualiza NavBar para usar o novo componente e ajusta estilos no cabeçalho.

--- src/components/header/nav-bar.tsx
+++ src/components/header/nav-bar.tsx
@@ -1,37 +1,70 @@
 "use client";
 import { Button } from "@/components/ui/button";
 import { NavBarItems } from "@/consts/nav-items";
+import { Menu } from "lucide-react";
 import { cn } from "@/lib/utils";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
 
-export default function NavBar() {
+const NavBarLink = () => {
   const pathname = usePathname();
   const isActive = (src: string) => {
     return pathname === src;
   };
 
   return (
+    <>
+      {NavBarItems.map((item) => (
+        <Button
+          key={item.title}
+          variant="link"
+          className="text-primary-foreground hover:text-primary/40 text-lg"
+        >
+          <Link
+            href={item.src}
+            className={cn(
+              `flex items-center gap-2`,
+              isActive(item.src) && "text-primary underline "
+            )}
+          >
+            {item.title}
+          </Link>
+        </Button>
+      ))}
+    </>
+  );
+};
+
+export default function NavBar() {
+  return (
     <nav className="flex items-center justify-between p-4 text-white">
       <div className="space-x-4 hidden md:flex">
-        {NavBarItems.map((item) => (
-          <Button
-            key={item.title}
-            variant="link"
-            className="text-primary-foreground hover:text-primary/40 text-lg"
-          >
-            <Link
-              href={item.src}
-              className={cn(
-                `flex items-center gap-2`,
-                isActive(item.src) && "text-primary underline "
-              )}
-            >
-              {item.title}
-            </Link>
-          </Button>
-        ))}
+        <NavBarLink />
       </div>
+      <Sheet>
+        <SheetTrigger asChild>
+          <Button variant={"ghost"} className="md:hidden">
+            <Menu className="size-8" />
+          </Button>
+        </SheetTrigger>
+        <SheetContent>
+          <SheetHeader>
+            <SheetTitle>Menu</SheetTitle>
+            <SheetDescription></SheetDescription>
+          </SheetHeader>
+          <div className="flex flex-col gap-2">
+            <NavBarLink />
+          </div>
+        </SheetContent>
+      </Sheet>
     </nav>
   );
 }
